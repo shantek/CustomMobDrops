76b5dd9cc827753895675da6711c391565089583
Kris <kris@shantek.com.au>
2024-05-22 11:45:27 +0930

    Fixed tab completer

--- src/main/java/io/shantek/Helpers/TabComplete.java
+++ src/main/java/io/shantek/Helpers/TabComplete.java
@@ -2,13 +2,15 @@ package io.shantek.Helpers;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class TabComplete {
+public class TabComplete implements TabCompleter {
 
+    @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
         if (args.length == 0 && alias.startsWith("shells")) {
             List<String> completions = new ArrayList();
