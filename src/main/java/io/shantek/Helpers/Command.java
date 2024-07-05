2e1dde8d12bd99b76b85517b20965b7dbb8166d0
Kris <kris@shantek.com.au>
2024-07-05 23:46:01 +0930

    Tweaks to commands

--- src/main/java/io/shantek/Helpers/Command.java
+++ src/main/java/io/shantek/Helpers/Command.java
@@ -5,6 +5,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class Command implements CommandExecutor {
 
@@ -22,31 +23,40 @@ public class Command implements CommandExecutor {
             return false;
         } else if (args.length > 0) {
             if (args[0].equalsIgnoreCase("reload")) {
-                if (sender.hasPermission("shantek.customdrops.reload")) {
+                if (sender.hasPermission("shantek.customdrops.reload") || sender.isOp()) {
                     customDrops.customDropConfig.loadConfig(sender);
                     functions.sendMessage(sender, "Customdrops config file reloaded.", false);
                     customDrops.getLogger().info("Customdrops config file reloaded.");
+                    return true;
                 } else {
                     functions.sendMessage(sender, "You do not have permission to reload the plugin.", true);
+                    return true;
                 }
             } else if (args[0].equalsIgnoreCase("enable")) {
-                if (sender.hasPermission("shantek.customdrops.enable")) {
+                if (sender.hasPermission("shantek.customdrops.enable") || sender.isOp()) {
                     customDrops.pluginConfig.setCustomDropsEnabled(true);
                     functions.sendMessage(sender, "Custom drops are now enabled.", false);
                     Bukkit.broadcastMessage(ChatColor.GREEN + "Custom drops are now enabled.");
+                    return true;
                 } else {
                     functions.sendMessage(sender, "You do not have permission to enable custom drops.", true);
+                    return true;
                 }
             } else if (args[0].equalsIgnoreCase("disable")) {
-                if (sender.hasPermission("shantek.customdrops.enable")) {
+                if (sender.hasPermission("shantek.customdrops.enable") || sender.isOp()) {
                     customDrops.pluginConfig.setCustomDropsEnabled(false);
                     functions.sendMessage(sender, "Custom drops are now disabled.", false);
                     Bukkit.broadcastMessage(ChatColor.RED + "Custom drops are now disabled.");
+                    return true;
                 } else {
                     functions.sendMessage(sender, "You do not have permission to disable custom drops.", true);
+                    return true;
                 }
             }
         }
+
+        // Show usage message if command is invalid
+        functions.sendMessage(sender, "Invalid command usage. Use /customdrops [reload | enable | disable]", true);
         return false;
     }
 }
